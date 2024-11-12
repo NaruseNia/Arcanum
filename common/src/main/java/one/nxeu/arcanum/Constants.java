2e9d3fb04f9a3a6e7e5a1b285b806c363936c11d
NaruseNia <notshinsin0000@gmail.com>
2024-11-12 14:48:39 +0900

    Setup

---
+++ common/src/main/java/one/nxeu/arcanum/Constants.java
@@ -0,0 +1,10 @@
+package one.nxeu.arcanum;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+    public static final String MOD_ID = "arcanum";
+    public static final String MOD_NAME = "Arcanum";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
